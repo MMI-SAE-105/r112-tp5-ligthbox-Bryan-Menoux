9e6d7c4bb32b9835fccc12d303c3fa3119070349
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2024-12-03 14:54:18 +0100

    ajout de la modification de l'image en fonction du target

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,15 @@
 // *** LIGHTBOX ***
 
 const lightBox = document.querySelector("#lightbox");
+const image = document.querySelector("#lightbox img");
 
 document.addEventListener("click", function (e) {
   if (lightBox.open) {
     lightBox.close();
   } else {
     lightBox.showModal();
+  }
+  if (e.target.matches("[data-full-img]")) {
+    image.src = e.target.dataset.fullImg;
   }
 });
