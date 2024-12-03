e8e84869129e6032888deaa7697091e0132967e0
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2024-12-03 14:15:45 +0100

    modifications de base du css

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // *** LIGHTBOX ***
+
+const lightBox = document.querySelector("#lightbox");
+
+lightBox.showModal();
+
+lightBox.close();
+
+document.body.addEventListener("click", function (evt) {
+  lightBox.showModal();
+  console.log("click");
+});
